d2179cdf9e6fa5d6c0ae808a311e719b0042890c
Y_Less <git@y-less.com>
2017-07-19 00:39:18 +0300

    Initial code.

---
+++ main.cpp
@@ -0,0 +1,261 @@
+#include <sampgdk/a_objects.h>
+#include <sampgdk/a_players.h>
+#include <sampgdk/a_samp.h>
+#include <sampgdk/a_vehicles.h>
+#include <sampgdk/core.h>
+#include <sampgdk/sdk.h>
+
+#include "main.h"
+
+extern void *pAMXFunctions;
+logprintf_t logprintf;
+
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports() {
+	return sampgdk::Supports() | SUPPORTS_AMX_NATIVES | SUPPORTS_PROCESS_TICK;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
+	pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
+	logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
+	return sampgdk::Load(ppData);
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL Unload() {
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX *amx) {
+	return AMX_ERR_NONE;
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX *amx) {
+	return AMX_ERR_NONE;
+}
+
+PLUGIN_EXPORT void PLUGIN_CALL ProcessTick() {
+	sampgdk::ProcessTick();
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeInit() {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnGameModeExit() {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerConnect(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerDisconnect(int playerid, int reason) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerSpawn(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerDeath(int playerid, int killerid, int reason) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleSpawn(int vehicleid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleDeath(int vehicleid, int killerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerText(int playerid, const char * text) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerCommandText(int playerid, const char * cmdtext) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestClass(int playerid, int classid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerEnterVehicle(int playerid, int vehicleid, bool ispassenger) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerExitVehicle(int playerid, int vehicleid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerStateChange(int playerid, int newstate, int oldstate) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerEnterCheckpoint(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerLeaveCheckpoint(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerEnterRaceCheckpoint(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerLeaveRaceCheckpoint(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnRconCommand(const char * cmd) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerRequestSpawn(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnObjectMoved(int objectid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerObjectMoved(int playerid, int objectid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerPickUpPickup(int playerid, int pickupid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleMod(int playerid, int vehicleid, int componentid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnEnterExitModShop(int playerid, int enterexit, int interiorid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehiclePaintjob(int playerid, int vehicleid, int paintjobid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleRespray(int playerid, int vehicleid, int color1, int color2) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleDamageStatusUpdate(int vehicleid, int playerid) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnUnoccupiedVehicleUpdate(int vehicleid, int playerid, int passenger_seat, float new_x, float new_y, float new_z, float vel_x, float vel_y, float vel_z) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerSelectedMenuRow(int playerid, int row) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerExitedMenu(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerInteriorChange(int playerid, int newinteriorid, int oldinteriorid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerKeyStateChange(int playerid, int newkeys, int oldkeys) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnRconLoginAttempt(const char * ip, const char * password, bool success) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerUpdate(int playerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerStreamIn(int playerid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerStreamOut(int playerid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleStreamIn(int vehicleid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleStreamOut(int vehicleid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnActorStreamIn(int actorid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnActorStreamOut(int actorid, int forplayerid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnDialogResponse(int playerid, int dialogid, int response, int listitem, const char * inputtext) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerTakeDamage(int playerid, int issuerid, float amount, int weaponid, int bodypart) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerGiveDamage(int playerid, int damagedid, float amount, int weaponid, int bodypart) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerGiveDamageActor(int playerid, int damaged_actorid, float amount, int weaponid, int bodypart) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerClickMap(int playerid, float fX, float fY, float fZ) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerClickTextDraw(int playerid, int clickedid) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerClickPlayerTextDraw(int playerid, int playertextid) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnIncomingConnection(int playerid, const char * ip_address, int port) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnTrailerUpdate(int playerid, int vehicleid) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnVehicleSirenStateChange(int playerid, int vehicleid, int newstate) {
+	return true;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerClickPlayer(int playerid, int clickedplayerid, int source) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerEditObject(int playerid, bool playerobject, int objectid, int response, float fX, float fY, float fZ, float fRotX, float fRotY, float fRotZ) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerEditAttachedObject(int playerid, int response, int index, int modelid, int boneid, float fOffsetX, float fOffsetY, float fOffsetZ, float fRotX, float fRotY, float fRotZ, float fScaleX, float fScaleY, float fScaleZ) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerSelectObject(int playerid, int type, int objectid, int modelid, float fX, float fY, float fZ) {
+	return false;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL OnPlayerWeaponShot(int playerid, int weaponid, int hittype, int hitid, float fX, float fY, float fZ) {
+	return true;
+}
+
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// C++ guidelines say that custom types should not end with "_t", because that
+// suffix is reserved for future standardised types.  Tell me a better suffix
+// then and I'll use that instead...  In general, I would make types lower-
+// case, variables camel-case, and functions pascal-case.  "logprintf" and
+// "logprintf_t" are exceptions to this, since they are so prevalent in SA:MP.
+typedef void (*logprintf_t)(char* format, ...);
+extern logprintf_t logprintf;
+
+#endif
+
