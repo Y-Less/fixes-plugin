b54ce162109f2e17dda2fc7cae8fed5f8a071ee8
Y_Less <git@y-less.com>
2017-07-20 02:15:54 +0300

    Macro for declaring fixes, while supporting conditional compilation.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,63 @@
 
 #include "main.h"
 
+struct fix_description_s
+{
+public:
+	char const * const Problem;
+	char const * const Solution;
+	char const * const See;
+	char const * const Author;
+	char const * const Topic;
+	char const * const FixedIn;
+
+	constexpr fix_description_s() : fix_description_s(0, 0, 0, 0, 0, 0) {}
+	constexpr fix_description_s(
+		char const * const problem,
+		char const * const solution,
+		char const * const see,
+		char const * const author,
+		char const * const topic,
+		char const * const fixedIn)
+	:
+		Problem(problem), Solution(solution), See(see), Author(author), Topic(topic), FixedIn(fixedIn) {}
+	constexpr fix_description_s problem(char const * const problem) { return { problem, Solution, See, Author, Topic, FixedIn }; }
+	constexpr fix_description_s solution(char const * const solution) { return { Problem, solution, See, Author, Topic, FixedIn }; }
+	constexpr fix_description_s see(char const * const see) { return { Problem, Solution, see, Author, Topic, FixedIn }; }
+	constexpr fix_description_s author(char const * const author) { return { Problem, Solution, See, author, Topic, FixedIn }; }
+	constexpr fix_description_s topic(char const * const topic) { return { Problem, Solution, See, Author, topic, FixedIn }; }
+	constexpr fix_description_s fixed_in(char const * const fixedIn) { return { Problem, Solution, See, Author, Topic, fixedIn }; }
+};
+
+template <int T>
+class DeclaredFix
+{
+public:
+	DeclaredFix(struct fix_description_s const description) {}
+};
+
+#define FIXES_IF__FIXES_DEFINED_ FIXES_NIL_,1
+#define FIXES_IF_0_FIXES_DEFINED_ FIXES_NIL_,0
+#define FIXES_IF_1_FIXES_DEFINED_ FIXES_NIL_,1
+
+#define FIXES_CHECK_DEFINED_(a) FIXES_CHECK_DEFINED_2_(a)
+#define FIXES_CHECK_DEFINED_2_(a) FIXES_CHECK_DEFINED_3_((FIXES_IF_##a##_FIXES_DEFINED_,2))
+
+#define FIXES_CHECK_DEFINED_3_(a) FIXES_CHECK_DEFINED_4_ a
+#define FIXES_CHECK_DEFINED_4_(ignore,result,...) result
+
+#define FIXES_REMOVE_BRACKETS_(...) __VA_ARGS__
+
+#define DEFINE_FIX(name,comments) \
+	DeclaredFix<FIXES_CHECK_DEFINED_(name)> FIX_##name##_(fix_description_s {} comments )
+
+DEFINE_FIX(GetPlayerColor,
+	.problem ("Returns \"0\" if \"SetPlayerColor\" has never been called.")
+	.solution("Call \"SetPlayerColor\" in \"OnPlayerConnect\".")
+	.see     ("\"OnPlayerConnect\".")
+	.author  ("\"KoczkaHUN\"")
+);
+
 extern void *pAMXFunctions;
 logprintf_t logprintf;
 
